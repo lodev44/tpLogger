2e7c14ce56bd85fa7389e1ed9b89ab7b8dfebb3c
Laurence Ott <lodev.diginamic@gmail.com>
2022-01-21 15:57:13 +0100

    Petites corrections

--- src/main/java/dev/console/App.java
+++ src/main/java/dev/console/App.java
@@ -3,6 +3,8 @@ package dev.console;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.service.AppService;
+
 public class App {
 
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
@@ -11,7 +13,8 @@ public class App {
 	// TODO Auto-generated method stub
 	LOG.info("Bonjour SLf4J !");
 	LOG.info("Implémentation Logback");
-
+	AppService testLogAppService = new AppService();
+	testLogAppService.executer("paramTest");
     }
 
 }
--- src/main/java/dev/service/AppService.java
+++ src/main/java/dev/service/AppService.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AppService {
-    private static final Logger LOG = LoggerFactory.getLogger(AppService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AppService.class); // pourrait être "toto" = nom du logger, nom arbitraire
 
     public void executer(String param) {
 	LOG.debug("Traitement 1 : param = {}", param);
