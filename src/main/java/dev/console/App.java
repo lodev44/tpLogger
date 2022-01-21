0fb26e3cc30ab4badc53432a695f9bb4e086f4f5
Laurence Ott <lodev.diginamic@gmail.com>
2022-01-21 14:43:39 +0100

    TP log slf4j

---
+++ src/main/java/dev/console/App.java
@@ -0,0 +1,17 @@
+package dev.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	LOG.info("Bonjour SLf4J !");
+	LOG.info("Implémentation Logback");
+
+    }
+
+}
